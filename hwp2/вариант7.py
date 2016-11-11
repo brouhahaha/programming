fe48c1472ae4a055a3faad3f6a76f6a268e86d5e
Maria <brouhahaha@yandex.ru>
2016-11-11 18:07:53 +0300

    deleted

--- hwp2/вариант7.py
+++
@@ -1,8 +0,0 @@
-print ('Введите число')
-n=int(input())
-k=1
-while k*2<=n:
-    k=k*2
-    print (k)
-    continue 
-    
--- hwp3/variant7.py
+++
@@ -1,9 +0,0 @@
-a=[]
-for i in range (8):
-        a.append(input())
-b=[]
-for i in range (0,7,2):
-        b.append(a[i])
-        b.append(a[i+1])
-        print (''.join(b))
-        b=[]
